87ed3c2bd5cc860cbdd59489ecbeb1365e34f079
Brian Hou <brianmhou@gmail.com>
2017-11-02 20:47:25 -0700

    Add hacky can TSR.

---
+++ include/pr_tsr/can.hpp
@@ -0,0 +1,59 @@
+#ifndef PR_TSR_CAN_HPP
+#define PR_TSR_CAN_HPP
+
+#include <Eigen/Core>
+#include <aikido/constraint/TSR.hpp>
+#include <dart/math/math.hpp>
+
+namespace pr_tsr
+{
+aikido::constraint::TSR getDefaultCanTSR()
+{
+  using Eigen::Isometry3d;
+  aikido::constraint::TSR tsr;
+
+  // Transform w.r.t root
+  tsr.mT0_w = Eigen::Isometry3d::Identity();
+
+  // Transform between end effector and w
+  double can_height = 0.123;
+  double can_radius = 0.033;
+  double push_distance = 0.05;
+  Isometry3d Tw_e = tsr.mT0_w;
+  Tw_e.translation() = Eigen::Vector3d{can_radius+push_distance, 0, can_height/2.0};
+  Eigen::Matrix3d rot;
+  rot << 0, 0, 1,
+        1, 0, 0,
+        0, 1, 0;
+  Tw_e.linear() = rot;
+  tsr.mTw_e = Tw_e;
+
+  // Rotation around object
+  Eigen::MatrixXd Bw = Eigen::Matrix<double, 6, 2>::Zero();
+  double vertical_tolerance = 0.02;
+  Bw(2, 0) = -vertical_tolerance;
+  Bw(2, 1) = vertical_tolerance;
+
+  // any side
+  // Bw(5, 0) = -M_PI;
+  // Bw(5, 1) = M_PI;
+
+  // right side
+  // Bw(5, 0) = -M_PI;
+  // Bw(5, 1) = 0;
+
+  // HERB side
+  // Bw(5, 0) = M_PI/2;
+  // Bw(5, 1) = 3*M_PI/2;
+
+  // HERB right side
+  Bw(5, 0) = M_PI;
+  Bw(5, 1) = 3*M_PI/2;
+  tsr.mBw = Bw;
+
+  // TODO: bias this so that we only sample regions that are easy for HERB to reach by pushing
+  return tsr;
+
+}
+}
+#endif // PR_TSR_CAN_HPP
